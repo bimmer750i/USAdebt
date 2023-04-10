468a67ca9412219c0463e4d1bb30c58677a17c5a
semio <15annmac@gmail.com>
2023-04-10 17:10:29 +0300

    Fixed Model

--- app/src/main/java/broz/tito/usadebt/data/remote/Model.kt
+++ app/src/main/java/broz/tito/usadebt/data/remote/Model.kt
@@ -41,57 +41,77 @@ class Model {
     suspend fun getDebt(): Flow<DebtResult> = flow {
         Log.d(TAG, "getting Debt")
         emit(PendingDebtResult())
-        val response = baseService.getDebt()
-        if (!response.isSuccessful) {
-            emit(FailureDebtResult())
-        }
-        else {
-            response.body()?.let {
-                val debt = it
-                emit(SuccessRawDebtResult(debt))
+        try {
+            val response = baseService.getDebt()
+            if (!response.isSuccessful) {
+                emit(FailureDebtResult())
             }
+            else {
+                response.body()?.let {
+                    val debt = it
+                    emit(SuccessRawDebtResult(debt))
+                }
+            }
+        }
+        catch (e: Exception) {
+            emit(FailureDebtResult())
         }
     }.flowOn(Dispatchers.IO)
 
     // Currency V1, mentioned in domain/data layer as currency_V1
     suspend fun getCurrency(): Flow<CurrencyResult> = flow {
         emit(PendingCurrencyResult())
-        val response = currencyV1Service.getCurrency()
-        if (!response.isSuccessful) {
-            emit(FailureCurrencyResult())
-        }
-        else {
-            response.body()?.let {
-                emit(SuccessRawCurrencyResult(it))
-            }
-        }
+       try {
+           val response = currencyV1Service.getCurrency()
+           if (!response.isSuccessful) {
+               emit(FailureCurrencyResult())
+           }
+           else {
+               response.body()?.let {
+                   emit(SuccessRawCurrencyResult(it))
+               }
+           }
+       }
+       catch (e: Exception) {
+           emit(FailureCurrencyResult())
+       }
     }.flowOn(Dispatchers.IO)
 
     suspend fun getCurrencyV2(): Flow<CurrencyV2Result> = flow {
         emit(PendingCurrencyV2Result())
-        val response = currencyV2Service.getCurrency_v2()
-        if (!response.isSuccessful) {
-            Log.d(TAG, response.code().toString())
-            emit(FailureCurrencyV2Result())
-        }
-        else {
-            response.body()?.let {
-                emit(SuccessRawCurrencyV2Result(it))
+        try {
+            val response = currencyV2Service.getCurrency_v2()
+            if (!response.isSuccessful) {
+                Log.d(TAG, response.code().toString())
+                emit(FailureCurrencyV2Result())
             }
+            else {
+                response.body()?.let {
+                    emit(SuccessRawCurrencyV2Result(it))
+                }
+            }
+        }
+        catch (e: Exception) {
+            emit(FailureCurrencyV2Result())
         }
     }.flowOn(Dispatchers.IO)
 
     suspend fun getHistory(days: String): Flow<HistoryResult> = flow {
         emit(PendingHistoryResult())
-        val response = baseService.getHistory(days)
-        if (!response.isSuccessful) {
-            emit(FailureHistoryResult())
-        }
-        else {
-            response.body()?.let { body ->
-                val result = parseFromRawHistory(body,this@Model)
-                emit(SuccessHistoryResult(result))
+        try {
+            val response = baseService.getHistory(days)
+            if (!response.isSuccessful) {
+                emit(FailureHistoryResult())
             }
+            else {
+                response.body()?.let { body ->
+                    val result = parseFromRawHistory(body,this@Model)
+                    emit(SuccessHistoryResult(result))
+                }
+            }
+        }
+        catch (e: Exception) {
+            emit(FailureHistoryResult())
         }
     }.flowOn(Dispatchers.IO)
 
